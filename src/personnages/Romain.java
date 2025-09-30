61b2f54e5060c1cc064da823790a180bf933c1d0
rmestre <raf3102@gmail.com>
2025-09-30 10:54:25 +0200

    Classe Romain écrite/générée

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,25 @@
 package personnages;
 
 public class Romain {
+	private String nom;
+	private int force;
+
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String text) {
+		System.out.println(prendreParole() + "\"" + text + "\"");
+	}
+
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return "Le romain " + nom + " : ";
+	}
 
 }
