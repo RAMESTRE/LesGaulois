c11ca7557e141b35134117881ff7e2dfb9953271
rmestre <raf3102@gmail.com>
2025-10-06 21:27:15 +0200

    TP1 1.1

---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,16 @@
+package test_fonctionnel;
+
+import personnages.Gaulois;
+
+public class TestGaulois {
+
+	public static void main() {
+		
+		Gaulois asterix, obelix;
+		
+		asterix = new Gaulois("Astérix", 8);
+		obelix = new Gaulois("Obélix", 16);
+
+	}
+
+}
