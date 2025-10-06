ad88133156cc38b97e82864c96167c892361e3cf
rmestre <raf3102@gmail.com>
2025-10-06 21:41:16 +0200

    TP1 1.2

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -4,13 +4,16 @@ import personnages.Gaulois;
 
 public class TestGaulois {
 
-	public static void main() {
-		
+	public static void main(String[] args) {
+
 		Gaulois asterix, obelix;
-		
+
 		asterix = new Gaulois("Astérix", 8);
 		obelix = new Gaulois("Obélix", 16);
 
+		asterix.parler("Bonjour Obélix");
+		obelix.parler("Bonjour Astérix? Ca te dirais d'aller chasser des sangliers ?");
+		asterix.parler("Oui très bonne idée.");
 	}
 
 }
