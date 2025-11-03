8a3db879520bb743abb23f616b49ce0abea0a9ef
rmestre <raf3102@gmail.com>
2025-11-03 18:51:09 +0100

    Fin du TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,12 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement = new Equipement[2];
+	private int nmEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -33,14 +37,37 @@ public class Romain {
 		assert isPostConditionVerified(oldForce);
 	}
 
+	public void sEquiper(Equipement nequipement) {
+		switch (nmEquipement) {
+		case 2:
+			System.out.println("Le soldat " + this.getNom() + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (this.equipement[nmEquipement - 1] == nequipement)
+				System.out.println("Le soldat " + this.getNom() + " possède déja " + nequipement.toString());
+			else {
+				ajoutTabEquip(nequipement);
+			}
+			break;
+		default:
+			ajoutTabEquip(nequipement);
+		}
+	}
+
+	private void ajoutTabEquip(Equipement nequipement) {
+		this.equipement[nmEquipement] = nequipement;
+		nmEquipement++;
+		System.out.println("Le soldat " + this.getNom() + " s'équipe avec un " + nequipement.toString());
+	}
+
 	private boolean isInvariantVerified() {
 		return this.force >= 0;
 	}
-	
+
 	private boolean isPreconditionVerified(int forceCoup) {
 		return forceCoup >= 0;
 	}
-	
+
 	private boolean isPostConditionVerified(int oldForce) {
 		return oldForce > this.force;
 	}
@@ -49,6 +76,14 @@ public class Romain {
 
 		Romain minus = new Romain("Minus", 6);
 		assert minus.isInvariantVerified();
+
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 
 	}
 
